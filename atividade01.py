1b8ea6fba28f8b728196f5d9b0770560dfff77fe
Peixoto-Vasc <jpvasconcelospeixoto30@gmail.com>
2024-09-09 09:42:30 -0300

    Minha primeira atidade

---
+++ atividade01.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+n1 = int(input('Digite o primeiro valor'))
+n2 = int(input('Digite o segundo valor'))
+
+soma = n1 + n2
+
+print(f'A soma de {n1} com {n2} é igual a {soma}')
